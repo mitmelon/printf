8693e1cf6f21b768e9d76555d212bdbfff5d1608
Sholawasiu <sholawasiu92@gmail.com>
2024-01-28 09:12:51 -0800

    converter prototype

--- converter.c
+++ converter.c
@@ -1,28 +1,28 @@
-#include <main.h>
+	#include <main.h>
 
-/**
- * convert - converts number and base into strings
- * @num: input number
- * @base: input base
- * @lowercase: flag if hexa value needs to be lowercase
- * Return: result string
- */
+	/**
+	 * convert - converts number and base into strings
+	 * @num: input number
+	 * @base: input base
+	 * @lowercase: flag if hexa value needs to be lowercase
+	 * Return: result string
+	 */
 
-char *convert(unsigned long int num, int base, int lowercase)
-{
-static char *rep;
-static char buffer[50];
-char *ptr;
+	char *convert(unsigned long int num, int base, int lowercase)
+	{
+	static char *rep;
+	static char buffer[50];
+	char *ptr;
 
-rep = (lowercase)
-	? "0123456789abcdef"
-	: "0123456789ABCDEF";
-*ptr = &buffer[49];
-*ptr = '\0';
-do {
-	*--ptr = rep[num % base];
-	num /= base;
-}
-while (num != 0);
-return (ptr);
-}
+	rep = (lowercase)
+		? "0123456789abcdef"
+		: "0123456789ABCDEF";
+	*ptr = &buffer[49];
+	*ptr = '\0';
+	do {
+		*--ptr = rep[num % base];
+		num /= base;
+	} while (num != 0);
+	return (ptr);
+	}
+
