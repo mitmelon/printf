4436eee77a802323cc3ec0013f0d4be741e78ade
Sholawasiu <sholawasiu92@gmail.com>
2024-01-28 11:04:08 -0800

    write char c to stdout

---
+++ write_funcs.c
@@ -0,0 +1,42 @@
+#include <main.h>
+#include <unistd.h>
+
+/**
+ * _putchar- writes the character c to the stdout
+ * @c: character to print
+ * Return: on success 1.
+ * on error, -1 is returned, and errno is set appropriately
+ * Description: _putchar uses a loacal buffer of 1024 to call write
+ * as little as possible
+ */
+int_putchar(char c)
+{
+	static charbbuffer[1024];
+	static int i;
+
+		if (c == -1 || i >= 1024)
+		{
+			write(1, &buff, i);
+			i = 0;
+		}
+		if (c != -1)
+		{
+			buff[i] = c;
+			i++;
+		}
+		return (1);
+}
+/**
+ * _puts- prints a string to stdout
+ * @string: pointer to the string of print
+ * Return: number of chas written
+ */
+int _puts(char *str)
+{
+	register int i;
+
+	for (i = 0, str[i] != '\0' ; i++)
+		_putchar (str[i]);
+	return (i);
+}
+
