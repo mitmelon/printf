b1660d4421846cd08e156a2c82c303fa0779dcda
Sholawasiu <sholawasiu92@gmail.com>
2024-01-30 11:14:49 -0800

    write char c to stdout

--- write_funcs.c
+++ write_funcs.c
@@ -11,17 +11,17 @@
  */
 int_putchar(char c)
 {
-	static charbbuffer[1024];
+	static char buf[1024];
 	static int i;
 
 	if (c == -1 || i >= 1024)
 	{
-		write(1, &buff, i);
+		write(1, &buf, i);
 		i = 0;
 	}
 	if (c != -1)
 	{
-		buff[i] = c;
+		buf[i] = c;
 		i++;
 	}
 	return (1);
