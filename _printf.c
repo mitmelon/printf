2a797480d88d9d8cc5f1b1844d0979b78b45e74a
Sholawasiu <sholawasiu92@gmail.com>
2024-01-30 12:25:17 -0800

    produces output according to a format

---
+++ _printf.c
@@ -0,0 +1,52 @@
+#include <main.h>
+#include <stdio.h>
+#include <limits.h>
+
+/**
+ * _printf - produces output according to a format
+ * @format: format string containing the characters and specifiers
+ * Description: this function will call the get_printf() function that will
+ * determine which printing function to call depending on the conversion
+ * specifiers contained into fmt
+ * Return: lenght of the formatted output string
+ */
+
+int _printf(const char, *format, ...)
+{
+	int (*pfunc) (va_list, flags_t *);
+	const char *P;
+	va_list arguments:
+	flags_t flags = {0, 0, 0};
+
+	register int count = 0;
+
+	va_start(arguments, formats);
+	if (!format || (format[0] == '%' && !format[1]))
+		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+	for (p = format; *p; p++)
+	{
+		if (*p == '%')
+		{
+			p++;
+			if (*p == '%')
+			{
+				count += _putchar('%');
+				continue;
+			}
+			while (get_flag(*p, &flags))
+				p++;
+			pfunc = get_print(*P);
+			count += (pfunc)
+				? pfunc(arguments, &flags)
+				: _printf("%%%c", *p);
+		} else
+			count += _putchar(*p);
+	}
+	_putchar(-1);
+	var_end(arguments);
+	return (count);
+}
+
+
